f3c612735fe5dc0acaaa74f2156942a56e3edc20
liuweiyin <liuwyok@sina.com>
2026-05-08 17:09:21 +0800

    修改 Prisma Client 的生成路径

--- src/plugins/prisma.ts
+++ src/plugins/prisma.ts
@@ -1,5 +1,5 @@
 import fp from 'fastify-plugin'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../generated/prisma-client'
 import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 
 function buildDatasourceUrl (): string {
